cc5932ff2d327a9ce380cf73b8f90b6f4e1692f7
CharlieDev <charlesgrimes90@gmail.com>
2018-02-10 18:47:10 +0000

    Initial set up of Spark

--- CanonicalTransformer/src/main/scala/grimes/besteventdate/datamodelling/transformer/CanonicalTransformerApplication.scala
+++ CanonicalTransformer/src/main/scala/grimes/besteventdate/datamodelling/transformer/CanonicalTransformerApplication.scala
@@ -1,13 +1,30 @@
 package grimes.besteventdate.datamodelling.transformer
 
+import java.io.File
+import java.io.File._
+
+import grimes.besteventdate.datamodelling.transformer.services.{CanonicalTransformerService, MeetupTransformerService}
 import grimes.besteventdate.scheduling.ScheduledApp
+import org.apache.spark.sql.SparkSession
 
 object CanonicalTransformerApplication {
-  def main(args: Array[String]): Unit = {
-
-  }
+  def main(args: Array[String]): Unit = new CanonicalTransformerApplication().init()
 }
 
 class CanonicalTransformerApplication extends ScheduledApp {
-  override def execute(): Boolean = ???
+  override def execute(): Boolean = {
+    val eventsJson: String = downloadEvents()
+
+    ingestEvents(eventsJson, new MeetupTransformerService, null)
+  }
+
+  private def downloadEvents(): String = ???
+
+  private def ingestEvents(eventsJson: String, transformerService: CanonicalTransformerService, sparkSession: SparkSession): Boolean = {
+    val eventsJsonFile: File = createTempFile("events", ".json")
+
+    //sparkSession.read
+
+    ???
+  }
 }
